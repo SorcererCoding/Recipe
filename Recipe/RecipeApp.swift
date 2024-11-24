f86e0c782afd10a771bc387813deadce54524e1c
SorcererCoding <SorcererCoding@gmail.com>
2024-11-24 13:21:36 -0600

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by Robert Huber on 11/24/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
